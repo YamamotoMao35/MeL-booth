39de4f34747511f10636ebcc3d08e12db69224d4
YamamotoMao35 <nokiroram@gmail.com>
2019-04-28 13:52:49 +0900

    Launch Mel-booth
    最初のコミットデータ

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,48 @@
+class TweetsController < ApplicationController
+
+  before_action :move_to_index, except: [:index]
+
+  def index
+    @tweets = Tweet.includes(:user).page(params[:page]).per(10).order('created_at DESC')
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+    @comments = @tweet.comments.includes(:user)
+  end
+
+  def new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.destroy
+    end
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+    end
+  end
+
+  private
+  def tweet_params
+    params.permit(:text,:image).merge(user_id:current_user.id)
+  end
+
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
+  end
+
+end
