ad301a0a33f3ed41bc997902e317b6eedff4d418
YamamotoMao35 <nokiroram@gmail.com>
2019-05-05 22:57:23 +0900

    Update users_controller.rb
    Update users_controller.rb
    
    usersコントローラにprivateメソッドが抜けていたため修正した。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
+    user = User.find(show_params[:id])
     @nickname = current_user.nickname
     @tweets = user.tweets.paginate(page: params[:page], per_page: 4).order("created_at DESC")
+  end
+
+  private
+  def show_params
+    params.permit(:id)
   end
 end
